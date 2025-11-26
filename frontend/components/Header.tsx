f0080124a9a191886cae474e43e1784b959a5bac
João Ferreira <joao.d.n.ferreira@tecnico.ulisboa.pt>
2025-11-26 18:38:42 +0000

    feat(frontend): added Header

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+	return (
+		<header className="p-4 border-b border-zinc-800 bg-zinc-900 flex justify-center items-center sticky top-0 z-50">
+			<h1 className="font-bold text-lg">
+				<span className="text-orange-500">AI</span> Recruiter
+			</h1>
+		</header>
+	);
+}
